c4e43622b03156833a55ec94288715e304c7bf74
Grom4K <47242169+Grom4K@users.noreply.github.com>
2021-03-07 23:03:46 +0200

    1

---
+++ Assets/Script/FollowPath.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
+
+
+
+//namespace Assets.Script
+//{
+//    class FollowPath : MonoBehaviour
+//    {
+//        // speed is the rate at which the object will rotate
+//        public float speed;
+
+//        void FixedUpdate()
+//        {
+//            // Сгенерируйте плоскость, пересекающую положение преобразования с восходящей нормалью.
+
+//            Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+//            // Сгенерируйте луч из положения курсора
+
+//            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+//            / Определите точку, в которой луч курсора пересекает плоскость.
+//// Это будет точка, в которую объект должен смотреть, чтобы смотреть на мышь.
+//// Raycasting к плоскому объекту дает нам только расстояние, поэтому нам придется взять это расстояние,
+/// затем найдите точку вдоль этого луча, которая встречается с этим расстоянием.В этом и будет смысл
+//// чтобы посмотреть.
+//            float hitdist = 0.0f;
+//            // If the ray is parallel to the plane, Raycast will return false.
+//            if (playerPlane.Raycast(ray, out hitdist))
+//            {
+//                // Get the point along the ray that hits the calculated distance.
+//                Vector3 targetPoint = ray.GetPoint(hitdist);
+
+//                // Определите вращение цели. Это вращение, если преобразование смотрит на целевую точку.
+//                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+//                // Плавно вращайтесь в направлении целевой точки.
+//                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+//            }
+//        }
+//    }
+//}
---
+++ Assets/Script/MovementPath.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour
+{
+    public enum PathTypes // типа пути
+    {
+        linear, //линейный  
+        loop, //зацыкленый
+    }
+
+    public enum MovementType //выбор типа движения
+    {
+        Moveing,
+        Lerping
+    }
+
+    public PathTypes PathType; //определить путь
+    public int movementDirection = 1; // направдление движения
+    public int moveingTo = 0; // точка к какой нуждно двигатся
+    public Transform[] Points; //масив из точек междду которыми двигается обект
+
+    public MovementType Type = MovementType.Moveing; // тип движения по пути
+    public MovementPath MyPath; // задать путь
+    public float speed = 1; // скорость движение по пути
+    public float maxDistanse = 0.1f; // растояние на которое обект должен подойти к точке, чтобы понять пто он на нужной точке
+
+    private IEnumerator<Transform> pointInPath;
+
+    void Start()
+    {
+        if (MyPath == null) // проверка прикреплен ли путь
+        {
+            Debug.Log("Примени путь");
+            return;
+        }
+
+        pointInPath = MyPath.GetNextPathPoint(); // обрашение к коротину GetNextPathPoint
+
+        pointInPath.MoveNext(); //получение следушей точки в пути
+
+        if (pointInPath.Current == null) // проверка точки к которой нужно двигатся
+        {
+            Debug.Log("Не задани точки");
+
+            return;
+        }
+
+        transform.position = pointInPath.Current.position;
+    }
+    void Update()
+    {
+        if (pointInPath == null || pointInPath.Current == null) // проверка отсутствия  пути
+        {
+            if (gameObject.name == "Points")
+            {
+                return;
+            }
+            else
+            {
+                Debug.Log("Не задан путь");  
+                return;
+            }
+
+        }
+
+        Vector3 targetPos = Vector3.zero;
+
+        if (Type == MovementType.Moveing)
+        {
+            targetPos = Vector3.MoveTowards(transform.position, pointInPath.Current.position, Time.deltaTime * speed); // движение обекта к следушей точки
+        }
+        else if (Type == MovementType.Lerping)
+        {
+            targetPos = Vector3.Lerp(transform.position, pointInPath.Current.position, Time.deltaTime * speed);
+        }
+
+        transform.LookAt(targetPos);
+        transform.position = targetPos;
+
+        var distanceSquer = (transform.position - pointInPath.Current.position).sqrMagnitude; //проверка дошли ли мы к точке чтобы двигатся к следушей точке
+        if (distanceSquer < maxDistanse * maxDistanse) // достаточно ли мы близко
+        {
+            pointInPath.MoveNext();
+        }
+    }
+
+
+
+    public void OnDrawGizmos() 
+    {
+        if (Points == null || Points.Length < 2) // проверка на наличие минимум 2 точек
+        {
+            return;
+        }
+        for (var i = 1; i < Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position); // рисуем линии между точкоми
+        }
+
+        if (PathType == PathTypes.loop) // если выбран замкнутый путь
+        {
+            Gizmos.DrawLine(Points[0].position, Points[Points.Length - 1].position); //нарисовать от 0 до последней точки
+
+        }
+
+    }
+    public IEnumerator<Transform> GetNextPathPoint() // получение положение следушей точки
+    {
+        if (Points == null || Points.Length < 1) // проверка точек
+        {
+            yield break; // позволяет выйти из коротина если нашел несоответствие
+        }
+
+        while (true)
+        {
+            yield return Points[moveingTo]; // возвращает текущее положение точки
+
+            if (Points.Length == 1) // если точка всего одна, выйти
+            {
+                continue;
+            }
+            if (PathType == PathTypes.linear) // если путь не зациклен
+            {
+                if (moveingTo <= 0) // если двигаемся по нарастающей
+                {
+                    movementDirection = 1; // добавим 1 к движению
+                }
+                else if (moveingTo >= Points.Length - 1) // если >= -1 двигаемся по убывающей
+                {
+                    movementDirection = -1; // убраем 1 из движения
+                }
+            }
+
+            moveingTo = moveingTo + movementDirection; // диапазон движения 
+
+            if (PathType == PathTypes.loop) // если путь зациклен
+            {
+                if (moveingTo >= Points.Length)
+                {
+                    moveingTo = 0;
+                }
+                if (moveingTo < 0)
+                {
+                    moveingTo = Points.Length - 1;
+                }
+            }
+        }
+    }
+
+}
+
